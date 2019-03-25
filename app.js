4fa573e190b5dc96e073e89bbcefef917dbed426
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-03-25 12:01:35 -0700

    added year of birth function

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-function createGreeting(name, age) {
-    const yearofBirth = 2019 - age;
-    return `"Hi my name is ${name} and i'm ${age} years old. I was born in ${yearofBirth}"`
+function getYearOfBirth(age) {
+    return 2019 - age;
 }
 
-const greeting1 = createGreeting();
+function createGreeting(name, age) {
+    const yob = getYearOfBirth();
+    return `"Hi my name is ${name} and i'm ${age} years old. I was born in ${yob}"`;
+}
+
+const year = getYearOfBirth(20);
+const greeting1 = createGreeting("brian", 20);
 console.log(greeting1)
 
