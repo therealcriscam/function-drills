a9d99bff122c78fc9a1b42deeb4989f8743a665d
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-03-25 14:30:20 -0700

    added daysInAMonth

--- app.js
+++ app.js
@@ -56,5 +56,62 @@ test;
 
 
 function decode(word) {
-  if (word)
+  if (word.charAt(0) === "a") {
+      return word.charAt(1);
+  }
+  else if (word.charAt(0) === "b") {
+      return word.charAt(2);
+  }
+  else if (word.charAt(0) === "c") {
+      return word.charAt(3);
+  }
+  else if (word.charAt(0) === "d") {
+      return word.charAt(4);
+  }
+  else {
+      return " ";
+  }
 }
+
+const testWord = decode("droop");
+console.log(testWord)
+
+function daysInAMonth(month, leapYear) {
+    
+    if (!month === "January" || "February" || "March" || "April" || "May" || "June" || "July" || "August" || "September" || "October" || "November" || "December") {
+        throw new Error("Must provide a valid month.");    
+    }
+    
+    switch (month, leapYear) {
+        case "January":
+        case "March":
+        case "May":
+        case "July":
+        case "August":
+        case "October":
+        case "December":
+        result = `${month} has 31 days`
+        break;
+        
+        case "April":
+        case "June":
+        case "September":
+        case "November":
+        result = `${month} has 30 days`
+        break;
+        
+        case "February", true: 
+        result = `${month} has 29 days` 
+        break;
+        
+        case "February", false: 
+        result = `${month} has 28 days` 
+        break;
+
+        // default: 
+        // throw new Error("Must provide a valid month.")
+    }
+}   
+
+const testMonth = daysInAMonth("February", true);
+console.log(testMonth);
