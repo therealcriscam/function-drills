2bf57f7422b9236e17fe5d6140593b5fe67e6595
Brian Sandoval <39751822+iambrianshin@users.noreply.github.com>
2019-03-25 15:44:22 -0400

    updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-function getYearOfBirth(age) {
-    return 2019 - age;
-}
+// function getYearOfBirth(age) {
+//   return 2019 - age;
+// }
 
-function createGreeting(name, age) {
-    const yob = getYearOfBirth();
-    return `"Hi my name is ${name} and i'm ${age} years old. I was born in ${yob}"`;
-}
+// function createGreeting(name, age) {
+//   const yob = getYearOfBirth(-10);
+//   return `"Hi my name is ${name} and i'm ${age} years old. I was born in ${yob}"`;
+// }
 
-const year = getYearOfBirth(20);
-const greeting1 = createGreeting("brian", 20);
-console.log(greeting1)
+// function yearOfBirth(age) {
+//   if (age < 0) {
+//     throw new Error("Age can not be negative");
+//   }
+//   return getYearOfBirth(-10);
+// }
 
+// try {
+//   const greeting1 = createGreeting("brian", -20);
+//   console.log(greeting1);
+// } catch (e) {
+//   console.error(e.message);
+// }
+
+// const year = getYearOfBirth(-10);
+// const greeting1 = createGreeting("brian", -10);
+// console.log(greeting1);
